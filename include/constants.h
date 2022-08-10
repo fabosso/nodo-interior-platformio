90145c75f2b591194215bc78242097db3ff6c4f4
Franco Abosso <franco.abosso@gm2dev.com>
2022-08-10 16:24:12 -0300

    refactor

--- include/constants.h
+++ include/constants.h
@@ -7,41 +7,41 @@
 */
 
 /// Comunicación serial.
-#define DEBUG_LEVEL 1				// Nivel de debug (0 inhabilita el puerto serial).
-#define SERIAL_BPS 9600				// Bitrate de las comunicaciones por puerto serial.
+#define DEBUG_LEVEL 1   // Nivel de debug (0 inhabilita el puerto serial).
+#define SERIAL_BPS 9600 // Bitrate de las comunicaciones por puerto serial.
 
 /// LoRa.
-#define LORA_FREQ 433175000 		// Frecuencia de la transmisión LoRa (en Hz).
-#define DEVICE_ID 10009 			// Identificador de este nodo.
-#define BROADCAST_ID (DEVICE_ID - DEVICE_ID % 10000 + 9999)   // ID broadcast para este tipo de nodo.
-#define DEVICE_ID_MAX_SIZE 6           // Tamaño máximo que se espera para cada DEVICE_ID entrante.
-#define INCOMING_PAYLOAD_MAX_SIZE 50   // Tamaño máximo esperado del payload LoRa entrante.
+#define LORA_FREQ 433175000                                                         // Frecuencia de la transmisión LoRa (en Hz).
+#define DEVICE_ID 10009                                                             // Identificador de este nodo.
+#define BROADCAST_ID (DEVICE_ID - DEVICE_ID % 10000 + 9999)                         // ID broadcast para este tipo de nodo.
+#define DEVICE_ID_MAX_SIZE 6                                                        // Tamaño máximo que se espera para cada DEVICE_ID entrante.
+#define INCOMING_PAYLOAD_MAX_SIZE 50                                                // Tamaño máximo esperado del payload LoRa entrante.
 #define INCOMING_FULL_MAX_SIZE (INCOMING_PAYLOAD_MAX_SIZE + DEVICE_ID_MAX_SIZE + 2) // Tamaño máximo esperado del mensaje entrante.
-#define MAX_SIZE_OUTCOMING_LORA_REPORT 200      // Tamaño máximo esperado del payload LoRa saliente.
-#define KNOWN_COMMANDS_SIZE 3       // Cantidad de comandos LoRa conocidos.
-#define LORA_TIMEOUT 20			    // Tiempo entre cada mensaje LoRa.
-#define LORA_SYNC_WORD 0x34			// Palabra de sincronización LoRa.
+#define MAX_SIZE_OUTCOMING_LORA_REPORT 200                                          // Tamaño máximo esperado del payload LoRa saliente.
+#define KNOWN_COMMANDS_SIZE 3                                                       // Cantidad de comandos LoRa conocidos.
+#define LORA_TIMEOUT 20                                                             // Tiempo entre cada mensaje LoRa.
+#define LORA_SYNC_WORD 0x34                                                         // Palabra de sincronización LoRa.
 
 /// Arrays.
-#define SENSORS_QTY 2               // Cantidad de sensores conectados.
-#define TIMEOUT_READ_SENSORS 2      // Tiempo entre mediciones.
+#define SENSORS_QTY 2          // Cantidad de sensores conectados.
+#define TIMEOUT_READ_SENSORS 2 // Tiempo entre mediciones.
 #define ARRAY_SIZE (LORA_TIMEOUT / TIMEOUT_READ_SENSORS + 3)
-#define TIMING_SLOTS 4 				// Cantidad de slots necesarios de timing (ver timing_helpers.h)
+#define TIMING_SLOTS 4 // Cantidad de slots necesarios de timing (ver timing_helpers.h)
 
 // Sensor de corriente.
-#define EMON_CROSSINGS 20           // Cantidad de semi-ondas muestreadas para medir tensión y/o corriente.
-#define EMON_TIMEOUT 1000           // Timeout de la rutina calcVI (en ms).
+#define EMON_CROSSINGS 20 // Cantidad de semi-ondas muestreadas para medir tensión y/o corriente.
+#define EMON_TIMEOUT 1000 // Timeout de la rutina calcVI (en ms).
 
 // Sensor de puerta abierta.
-#define PUERTA_ACTIVA HIGH          // Señal entrante cuando la puerta está abierta.
-#define PUERTA_INACTIVA LOW         // Señal entrante cuando la puerta está cerrada.
+#define PUERTA_ACTIVA HIGH  // Señal entrante cuando la puerta está abierta.
+#define PUERTA_INACTIVA LOW // Señal entrante cuando la puerta está cerrada.
 
 // Sensor de boton antipánico.
-#define ANTIPANICO_ACTIVO HIGH      // Señal entrante cuando el boton está presionado.
-#define ANTIPANICO_INACTIVO LOW     // Señal entrante cuando el boton no está presionado.
+#define ANTIPANICO_ACTIVO HIGH  // Señal entrante cuando el boton está presionado.
+#define ANTIPANICO_INACTIVO LOW // Señal entrante cuando el boton no está presionado.
 
 /// Valores mock.
 // #define TENSION_MOCK 223.11      // Tensión de prueba.
 #define TEMPERATURA_MOCK 23.11      // Temperatura de prueba.
-// #define ANTIPANICO_MOCK 0           // Antipánico de prueba.
-// #define PUERTA_MOCK 0               // Puerta de prueba.
+// #define ANTIPANICO_MOCK 0        // Antipánico de prueba.
+// #define PUERTA_MOCK 0            // Puerta de prueba.
